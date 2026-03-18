5f5869e91db90414ead5406aa409118634b1df26
timothy1202 <timothy1202@naver.com>
2026-03-18 14:47:05 +0900

    기본 프로젝트 커밋

---
+++ Source/OW_System/OW_System.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "OW_System.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, OW_System, "OW_System" );
